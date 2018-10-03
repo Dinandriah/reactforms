cb1f0851ace69cbb6cea98a698e7f1789c94f54b
Paul Barrett <paulbarrett@Pauls-MacBook-Air.local>
2018-10-03 11:19:50 -0600

    first commit

--- src/forms/Home.js
+++ src/forms/Home.js
@@ -13,12 +13,6 @@ const BasicExample = () => (
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-        <li>
           <Link to="/CreateLogin">Create Login</Link>
         </li>
         <li>
@@ -32,8 +26,6 @@ const BasicExample = () => (
       <hr />
 
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
       <Route path="/CreateLogin" component={CreateLogin} />
       <Route path="/login" component={login} />
       <Route path="/forgot" component={forgot} />
@@ -47,40 +39,9 @@ const Home = () => (
   </div>
 );
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
+ 
 
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
 
 export default BasicExample;
